188624e1586e95d0df38b9eab621ade5634460bc
Nicolas Frankel <nicolas@frankel.ch>
2019-10-13 12:15:29 +0200

    Cleanup

--- stream-dynamic/src/main/kotlin/com/hazelcast/jet/swisstrain/data/CustomBuilder.kt
+++ stream-dynamic/src/main/kotlin/com/hazelcast/jet/swisstrain/data/CustomBuilder.kt
@@ -1,5 +1,7 @@
 package com.hazelcast.jet.swisstrain.data
 
+import java.io.Serializable
+import java.time.Instant
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.result.Result
 import com.hazelcast.jet.core.Processor
@@ -7,21 +9,19 @@ import com.hazelcast.jet.function.BiConsumerEx
 import com.hazelcast.jet.function.FunctionEx
 import com.hazelcast.jet.pipeline.SourceBuilder
 import com.hazelcast.jet.pipeline.SourceBuilder.SourceBuffer
-import java.io.Serializable
-import java.time.Instant
-
-private const val PATTERN = "{\"id\":\""
 
 fun remoteService(url: String, token: String) = SourceBuilder
     .stream("http-source", CreateContext(url))
-    .fillBufferFn<String>(FillBuffer(token))
+    .fillBufferFn(FillBuffer(token))
     .build()
 
 class FillBuffer(private val token: String) : BiConsumerEx<Pair<String, TimeHolder>, SourceBuffer<String>> {
     override fun acceptEx(tuple: Pair<String, TimeHolder>, buffer: SourceBuffer<String>) {
         if (Instant.now().isAfter(tuple.second.value.plusSeconds(30))) {
             val (_, _, result) =
-                Fuel.get(tuple.first).header("Authorization" to token).responseString()
+                Fuel.get(tuple.first).header(
+                    "Authorization" to token,
+                    "Accept-Encoding" to "gzip, deflate").responseString()
             when (result) {
                 is Result.Failure -> println(result.getException())
                 is Result.Success -> buffer.add(result.get())
