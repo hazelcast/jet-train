0f77da39469a540a369b7644019b4036141cc668
Nicolas Frankel <nicolas@frankel.ch>
2020-01-08 15:25:23 -0500

    Update reference data to latest version

--- load-static/src/main/kotlin/com/hazelcast/jet/swisstrain/refs/CustomBuilder.kt
+++ load-static/src/main/kotlin/com/hazelcast/jet/swisstrain/refs/CustomBuilder.kt
@@ -35,6 +35,6 @@ class CloseReader : ConsumerEx<BufferedReader> {
 class CreateReader(private val filename: String) : FunctionEx<Context, BufferedReader> {
     override fun applyEx(ctx: Context) =
         BufferedReader(
-            FileReader(File("/opt/hazelcast/data/gtfsfp20192019-12-11/$filename.txt"))
+            FileReader(File("/opt/hazelcast/data/gtfsfp20202020-01-08/$filename.txt"))
         )
 }
