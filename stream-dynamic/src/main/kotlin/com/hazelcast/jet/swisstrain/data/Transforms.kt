fe1cbb34ff939dde301178d3fb8f0e26db810367
Nicolas Frankel <nicolas@frankel.ch>
2020-01-12 02:34:15 -0500

    Close to completion

--- stream-dynamic/src/main/kotlin/com/hazelcast/jet/swisstrain/data/Transforms.kt
+++ stream-dynamic/src/main/kotlin/com/hazelcast/jet/swisstrain/data/Transforms.kt
@@ -5,8 +5,8 @@ import com.hazelcast.internal.json.JsonArray
 import com.hazelcast.internal.json.JsonObject
 import com.hazelcast.jet.Traverser
 import com.hazelcast.jet.function.FunctionEx
+import com.hazelcast.jet.function.ToLongFunctionEx
 import java.io.Serializable
-import java.time.LocalTime
 import java.util.*
 
 class SplitPayload : FunctionEx<String, Traverser<JsonObject>> {
@@ -24,118 +24,14 @@ class TripTraverser(entities: JsonArray) : Traverser<JsonObject>, Serializable {
         else null
 }
 
-class AdjustTimeWithDelays : FunctionEx<JsonObject, JsonObject> {
-    override fun applyEx(json: JsonObject): JsonObject {
-        json["trip_update"]?.asObject()
-            ?.get("stop_time_update")?.asArray()
-            ?.map { it.asObject() }
-            ?.forEach {
-                it.let { stopTimeUpdate ->
-                    computeTime(stopTimeUpdate, "departure")
-                    computeTime(stopTimeUpdate, "arrival")
-                }
-            }
-        return json
-    }
-
-    private fun computeTime(stopTimeUpdate: JsonObject, name: String) {
-        stopTimeUpdate[name]?.asObject()?.get("delay")?.let {
-            val delay = it.asLong()
-            val stopTimeString = stopTimeUpdate.getString("stop_time_$name", null)
-            if (stopTimeString != null) {
-                // Sometimes, time follows this pattern: '24:21:00'
-                val fixedStopTimeString = stopTimeString.replace("^24".toRegex(), "00")
-                val time = LocalTime.parse(fixedStopTimeString)
-                val newTime = time.plusSeconds(delay)
-                stopTimeUpdate.set("${name}_time", newTime.toString())
-            }
-        }
-    }
-}
-
-class ComputeLocation : FunctionEx<JsonObject, JsonObject> {
-    override fun applyEx(json: JsonObject): JsonObject? {
-        val stopTimeUpdates = json["trip_update"]?.asObject()
-            ?.get("stop_time_update")?.asArray()
-        curate(stopTimeUpdates)
-        return when {
-            stopTimeUpdates == null -> null
-            stopTimeUpdates.isEmpty -> null
-            stopTimeUpdates.size() == 1 -> null
-            stopTimeUpdates.size() == 2 -> toMoving(stopTimeUpdates, 0, 1)
-            else -> {
-                val computedStops = findStops(stopTimeUpdates)
-                return json.merge(computedStops)
-                    .remove("trip_update")
-            }
-        }
-    }
-
-    // Because sometimes, intermediate stops have neither arrival nor departure
-    // And yes, it's ugly how it's handled, but I have no other ideas right now
-    // PR welcome
-    private fun curate(stopTimeUpdates: JsonArray?) {
-        val badIndices = mutableListOf<Int>()
-        stopTimeUpdates?.forEachIndexed { index, item ->
-            val stopTimeUpdate = item.asObject()
-            if (!isDataSafe(stopTimeUpdate)) {
-                badIndices.add(index)
-            }
-        }
-        badIndices.reversed().forEach {
-            stopTimeUpdates?.remove(it)
-        }
-    }
-
-    private fun findStops(stopTimeUpdates: JsonArray): JsonObject {
-        val now = LocalTime.now()
-        val bound = stopTimeUpdates.size() - 1
-        var previous: JsonObject
-        var next: JsonObject
-        for (i in 0 until bound) {
-            previous = stopTimeUpdates.get(i).asObject()
-            next = stopTimeUpdates.get(i + 1).asObject()
-            val previousArrival = arrivalTime(previous)
-            val previousDeparture = departureTime(previous)
-            val nextArrival = arrivalTime(next)
-            if (now.isBefore(previousDeparture) && now.isAfter(previousArrival)) {
-                return toStopped(stopTimeUpdates, i, i + 1)
-            }
-            if (now.isAfter(previousDeparture) && now.isBefore(nextArrival)) {
-                return toMoving(stopTimeUpdates, i, i + 1)
-            }
-        }
-        return toStopped(stopTimeUpdates, bound - 1, bound)
-    }
-
-    private fun arrivalTime(stopTimeUpdate: JsonObject) =
-        time(stopTimeUpdate, "arrival_time")
-
-    private fun departureTime(stopTimeUpdate: JsonObject) =
-        time(stopTimeUpdate, "departure_time")
-
-    private fun time(stopTimeUpdate: JsonObject, which: String): LocalTime? {
-        val timeString = stopTimeUpdate.getString(which, null)
-        return if (timeString == null) null
-        else LocalTime.parse(timeString)
-    }
-
-    private fun isDataSafe(json: JsonObject) =
-        json.get("arrival_time") != null || json.get("arrival_time") != null
-
-    private fun toStopped(stopTimeUpdates: JsonArray, from: Int, to: Int) =
-        toStatus(stopTimeUpdates, from, to, "STOPPED")
-
-    private fun toMoving(stopTimeUpdates: JsonArray, from: Int, to: Int) =
-        toStatus(stopTimeUpdates, from, to, "MOVING")
-
-    private fun toStatus(stopTimeUpdates: JsonArray, from: Int, to: Int, status: String) =
-        JsonObject().add("from", stopTimeUpdates.get(from))
-            .add("to", stopTimeUpdates.get(to))
-            .add("status", status)
-}
-
 class ToEntry : FunctionEx<JsonObject, Map.Entry<String, JsonObject>> {
     override fun applyEx(json: JsonObject): Map.Entry<String, JsonObject> =
         AbstractMap.SimpleEntry(json.getString("id", null), json)
+}
+
+class TimestampExtractor : ToLongFunctionEx<String> {
+    override fun applyAsLongEx(value: String): Long {
+        val json = Json.parse(value).asObject()
+        return json.get("header").asObject().getLong("timestamp", -1)
+    }
 }
