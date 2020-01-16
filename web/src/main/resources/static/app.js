159c735e9160470d1917cd7c87c1b04943c6e42e
Andrey Goncharov <andrey@goncharov.page>
2020-01-16 17:57:15 +0300

    Add semicolon

--- web/src/main/resources/static/app.js
+++ web/src/main/resources/static/app.js
@@ -1,4 +1,4 @@
-const routeDefaultColor = '#808080'
+const routeDefaultColor = '#808080';
 
 const randomColor = () => {
   const colors = [
@@ -8,167 +8,167 @@ const randomColor = () => {
     '#FF0000',
     '#00FFFF',
     '#FF0000',
-  ]
-  return colors[Math.trunc(Math.random() * colors.length)]
-}
+  ];
+  return colors[Math.trunc(Math.random() * colors.length)];
+};
 
-const currentTime = () => Date.now() / 1000
+const currentTime = () => Date.now() / 1000;
 
 class Route {
   static stopToLatLong({ latitude, longitude }) {
-    return [latitude, longitude]
+    return [latitude, longitude];
   }
 
   constructor(map, schedule) {
     this.polyline = L.polyline(schedule.map(Route.stopToLatLong), {
       color: routeDefaultColor,
-    })
-    this.polyline.addTo(map)
+    });
+    this.polyline.addTo(map);
     this.stops = schedule.map(({ stop, latitude, longitude }) => {
       const circle = L.circle([latitude, longitude], {
         color: routeDefaultColor,
         radius: 10,
         fill: true,
         fillOpacity: 0.8,
-      })
-      circle.bindTooltip(stop)
-      circle.addTo(map)
-      return circle
-    })
+      });
+      circle.bindTooltip(stop);
+      circle.addTo(map);
+      return circle;
+    });
   }
 
   setColor(newColor) {
-    this.polyline.setStyle({ color: newColor })
-    this.stops.forEach((stop) => stop.setStyle({ color: newColor }))
+    this.polyline.setStyle({ color: newColor });
+    this.stops.forEach((stop) => stop.setStyle({ color: newColor }));
   }
 
   remove() {
-    this.polyline.remove()
-    this.stops.forEach((stop) => stop.remove())
+    this.polyline.remove();
+    this.stops.forEach((stop) => stop.remove());
   }
 }
 
 class Train {
   constructor(map, routeId, schedule, name, onFinalStopCb) {
-    this.routeId = routeId
-    this.schedule = schedule
-    this.name = name
+    this.routeId = routeId;
+    this.schedule = schedule;
+    this.name = name;
 
-    this._map = map
-    this._onFinalStopCb = onFinalStopCb
-    this._route = new Route(this._map, this.schedule)
-    this._train = undefined
-    this._heartbeatIntervalId = undefined
+    this._map = map;
+    this._onFinalStopCb = onFinalStopCb;
+    this._route = new Route(this._map, this.schedule);
+    this._train = undefined;
+    this._heartbeatIntervalId = undefined;
 
-    this._createHeartbeat()
+    this._createHeartbeat();
   }
 
   updateSchedule(newSchedule) {
-    this.schedule = newSchedule
-    this._refresh()
+    this.schedule = newSchedule;
+    this._refresh();
   }
 
   _createRoute() {}
 
   _createHeartbeat() {
-    this._heartbeatIntervalId = setInterval(() => this._refresh(), 1000)
-    this._refresh()
+    this._heartbeatIntervalId = setInterval(() => this._refresh(), 1000);
+    this._refresh();
   }
 
   _refresh() {
     if (this._hasMovementEnded) {
-      this._onFinalStop()
-      return
+      this._onFinalStop();
+      return;
     }
 
     if (!this._hasMovementStarted) {
-      return
+      return;
     }
 
     if (!this._train) {
-      this._createNewTrain()
-      this._route.setColor(randomColor())
-      return
+      this._createNewTrain();
+      this._route.setColor(randomColor());
+      return;
     }
 
-    this._train.setLatLng(this._currentLatLong)
+    this._train.setLatLng(this._currentLatLong);
   }
 
   _createNewTrain() {
-    this._train = L.marker(this._currentLatLong)
-    this._train.bindTooltip(this.name)
-    this._train.addTo(this._map)
+    this._train = L.marker(this._currentLatLong);
+    this._train.bindTooltip(this.name);
+    this._train.addTo(this._map);
   }
 
   _onFinalStop() {
     if (this._train) {
-      this._train.remove()
-      this._train = undefined
+      this._train.remove();
+      this._train = undefined;
     }
 
     if (this._route) {
-      this._route.remove()
-      this._route = undefined
+      this._route.remove();
+      this._route = undefined;
     }
 
-    clearInterval(this._heartbeatIntervalId)
+    clearInterval(this._heartbeatIntervalId);
 
-    this._onFinalStopCb(this)
+    this._onFinalStopCb(this);
   }
 
   get _currentLatLong() {
     if (!this._hasMovementStarted) {
-      return Route.stopToLatLong(this.schedule[0])
+      return Route.stopToLatLong(this.schedule[0]);
     }
 
-    const t = currentTime()
+    const t = currentTime();
 
-    const nextStopI = this.schedule.findIndex(({ arrival }) => t < arrival)
+    const nextStopI = this.schedule.findIndex(({ arrival }) => t < arrival);
 
     if (nextStopI === -1) {
       // Train has arrived at the final stop
-      return Route.stopToLatLong(this.schedule[this.schedule.length - 1])
+      return Route.stopToLatLong(this.schedule[this.schedule.length - 1]);
     }
 
-    const nextStop = this.schedule[nextStopI]
-    const prevStop = this.schedule[nextStopI - 1]
+    const nextStop = this.schedule[nextStopI];
+    const prevStop = this.schedule[nextStopI - 1];
 
     if (top < prevStop.departure) {
       // Train hasn't departed yet
-      return Route.stopToLatLong(prevStop)
+      return Route.stopToLatLong(prevStop);
     }
 
     const distancePassed =
-      (t - prevStop.departure) / (nextStop.arrival - prevStop.departure)
+      (t - prevStop.departure) / (nextStop.arrival - prevStop.departure);
 
-    const { latitude: prevLat, longitude: prevLong } = prevStop
-    const { latitude: nextLat, longitude: nextLong } = nextStop
+    const { latitude: prevLat, longitude: prevLong } = prevStop;
+    const { latitude: nextLat, longitude: nextLong } = nextStop;
 
-    const currentLat = prevLat + (nextLat - prevLat) * distancePassed
-    const currentLong = prevLong + (nextLong - prevLong) * distancePassed
+    const currentLat = prevLat + (nextLat - prevLat) * distancePassed;
+    const currentLong = prevLong + (nextLong - prevLong) * distancePassed;
 
-    return [currentLat, currentLong]
+    return [currentLat, currentLong];
   }
 
   get _hasMovementEnded() {
-    const routeEndTime = this.schedule[this.schedule.length - 1].arrival
-    return currentTime() > routeEndTime
+    const routeEndTime = this.schedule[this.schedule.length - 1].arrival;
+    return currentTime() > routeEndTime;
   }
 
   get _hasMovementStarted() {
-    const routeStartTime = this.schedule[0].arrival
-    return currentTime() > routeStartTime
+    const routeStartTime = this.schedule[0].arrival;
+    return currentTime() > routeStartTime;
   }
 }
 
-const falseIds = []
+const falseIds = [];
 
 class Container {
   constructor() {
-    this.map = L.map('map').setView([46.819382, 8.416515], 9)
-    this._trains = {}
-    this._socket = undefined
-    this._stomp = undefined
+    this.map = L.map('map').setView([46.819382, 8.416515], 9);
+    this._trains = {};
+    this._socket = undefined;
+    this._stomp = undefined;
   }
 
   initialize() {
@@ -179,23 +179,23 @@ class Container {
           '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,' +
           '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
       },
-    ).addTo(this.map)
+    ).addTo(this.map);
 
-    this._socket = new SockJS('/hazelcast')
-    this._stomp = Stomp.over(this._socket)
-    this._stomp.reconnect_delay = 2000
+    this._socket = new SockJS('/hazelcast');
+    this._stomp = Stomp.over(this._socket);
+    this._stomp.reconnect_delay = 2000;
     this._stomp.connect({}, () => {
       this._stomp.subscribe('/topic/updates', (update) => {
-        const data = JSON.parse(update.body)
+        const data = JSON.parse(update.body);
         data.schedule.forEach((stop) => {
-          stop.longitude = parseFloat(stop.longitude)
-          stop.latitude = parseFloat(stop.latitude)
-        })
-        this._processData(data)
-      })
-    })
+          stop.longitude = parseFloat(stop.longitude);
+          stop.latitude = parseFloat(stop.latitude);
+        });
+        this._processData(data);
+      });
+    });
 
-    $.ajax('/data/')
+    $.ajax('/data/');
   }
 
   _processData({
@@ -206,10 +206,10 @@ class Container {
     agency_name: agencyName,
   }) {
     if (currentTime() > schedule[schedule.length - 1].arrival) {
-      return
+      return;
     }
 
-    const existingTrain = this._trains[routeId]
+    const existingTrain = this._trains[routeId];
 
     if (!existingTrain) {
       const newTrain = new Train(
@@ -218,17 +218,17 @@ class Container {
         schedule,
         `${routeType} ${routeName} (${agencyName})`,
         (train) => this._onTrainFinalStop(train),
-      )
-      this._trains[routeId] = newTrain
-      return
+      );
+      this._trains[routeId] = newTrain;
+      return;
     }
 
-    existingTrain.updateSchedule(schedule)
+    existingTrain.updateSchedule(schedule);
   }
 
   _onTrainFinalStop(train) {
-    delete this._trains[train.routeId]
+    delete this._trains[train.routeId];
   }
 }
 
-new Container().initialize()
+new Container().initialize();
