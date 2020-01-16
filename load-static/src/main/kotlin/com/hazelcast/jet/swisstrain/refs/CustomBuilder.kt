20d139828bab66903442f23aaa118381eab3deba
Nicolas Frankel <nicolas@frankel.ch>
2020-01-16 23:19:33 +0300

    Update static data with current week's data files

--- load-static/src/main/kotlin/com/hazelcast/jet/swisstrain/refs/CustomBuilder.kt
+++ load-static/src/main/kotlin/com/hazelcast/jet/swisstrain/refs/CustomBuilder.kt
@@ -35,6 +35,6 @@ class CloseReader : ConsumerEx<BufferedReader> {
 class CreateReader(private val filename: String) : FunctionEx<Context, BufferedReader> {
     override fun applyEx(ctx: Context) =
         BufferedReader(
-            FileReader(File("/opt/hazelcast/data/gtfsfp20202020-01-08/$filename.txt"))
+            FileReader(File("/opt/hazelcast/data/gtfsfp20202020-01-15/$filename.txt"))
         )
 }
