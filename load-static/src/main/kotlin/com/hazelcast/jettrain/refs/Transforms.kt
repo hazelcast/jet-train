89662acfd143715a929e21d7c1f78e43ca8a5146
Nicolas Frankel <nicolas@frankel.ch>
2021-01-20 11:56:30 +0100

    Fix stop times handling

--- load-static/src/main/kotlin/com/hazelcast/jettrain/refs/Transforms.kt
+++ load-static/src/main/kotlin/com/hazelcast/jettrain/refs/Transforms.kt
@@ -1,32 +1,24 @@
 package com.hazelcast.jettrain.refs
 
-import com.hazelcast.core.HazelcastJsonValue
 import com.hazelcast.function.FunctionEx
 import com.hazelcast.internal.json.Json
-import com.hazelcast.internal.json.JsonValue
-import com.hazelcast.jet.Traverser
+import com.hazelcast.internal.json.JsonObject
 import com.hazelcast.jet.Util
 import java.io.Serializable
 
 object ToEntry : FunctionEx<String?, Map.Entry<Serializable, Any>> {
     override fun applyEx(string: String?): Map.Entry<Serializable, Any>? {
-        val id: JsonValue? = string?.let(Json::parse)
+        val id = string?.let(Json::parse)
             ?.asObject()
-            ?.get("id")
-        return when (id?.isString) {
-            null -> null
-            true -> Util.entry(id.asString(), string)
-            false -> Util.entry(id.toString(), HazelcastJsonValue(string))
-        }
+            ?.getString("id", null)
+        return if (id != null) Util.entry(id, string)
+        else null
     }
 }
 
-object MapTraverser : FunctionEx<Map<String, List<String>>, Traverser<Pair<String, List<String>>>> {
-    override fun applyEx(map: Map<String, List<String>>): Traverser<Pair<String, List<String>>> {
-        val iterator = map.iterator()
-        return Traverser {
-            if (iterator.hasNext()) iterator.next().toPair()
-            else null
-        }
-    }
+object ToEntryForStopTime : FunctionEx<JsonObject, Map.Entry<String, String>> {
+    override fun applyEx(json: JsonObject): Map.Entry<String, String> = Util.entry(
+        json.get("id").asString(),
+        json.get("schedule").asArray().toString()
+    )
 }
