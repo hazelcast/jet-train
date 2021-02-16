8a47c3c7eb76fd653c0d3f2c904b70c3d0ddb8f7
Nicolas Frankel <nicolas@frankel.ch>
2021-02-16 09:26:49 +0100

    Code cleanup

--- stream-dynamic/src/main/kotlin/com/hazelcast/jettrain/data/Merger.kt
+++ stream-dynamic/src/main/kotlin/com/hazelcast/jettrain/data/Merger.kt
@@ -1,7 +1,6 @@
 package com.hazelcast.jettrain.data
 
 import com.google.gson.JsonArray
-import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import com.hazelcast.function.BiFunctionEx
