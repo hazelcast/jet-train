2f663609db0bfdb8d06af0c11244f284bdbb780f
Nicolas Frankel <nicolas@frankel.ch>
2021-03-25 11:33:45 +0100

    Replace custome file reading by the one provided OOTB by Jet 4.4

--- load-static/src/main/kotlin/com/hazelcast/jettrain/refs/Pipelines.kt
+++ load-static/src/main/kotlin/com/hazelcast/jettrain/refs/Pipelines.kt
@@ -1,17 +1,34 @@
 package com.hazelcast.jettrain.refs
 
-import com.hazelcast.function.FunctionEx
 import com.hazelcast.jet.aggregate.AggregateOperations
 import com.hazelcast.jet.pipeline.Pipeline
 import com.hazelcast.jet.pipeline.Sinks
+import com.hazelcast.jet.pipeline.file.FileFormat
+import com.hazelcast.jet.pipeline.file.FileSources
 
-internal val stops = pipeline("stops", ToStop)
-internal val agencies = pipeline("agency", ToAgency)
-internal val routes = pipeline("routes", ToRoute)
-internal val trips = pipeline("trips", ToTrip)
+internal val stops = pipeline(
+    "stops",
+    listOf("stop_id", "stop_name", "stop_lat", "stop_lon"),
+    listOf("id", "stop_name", "stop_lat", "stop_long")
+)
+internal val agencies = pipeline(
+    "agency",
+    listOf("agency_id", "agency_name"),
+    listOf("id", "agency_name")
+)
+internal val routes = pipeline(
+    "routes",
+    listOf("route_id", "agency_id", "route_short_name", "route_type", "route_color"),
+    listOf("id", "agencyId", "route_name", "route_type", "route_color")
+)
+internal val trips = pipeline(
+    "trips",
+    listOf("trip_id", "route_id", "trip_headsign"),
+    listOf("id", "routeId", "trip_headsign")
+)
 internal val stopTimes = Pipeline.create().apply {
-    readFrom(file("stop_times"))
-        .groupingKey { it.split(",")[0] }
+    readFrom(file("stop_times", listOf("trip_id", "arrival_time", "departure_time", "stop_id", "stop_sequence")))
+        .groupingKey { it[0] }
         .aggregate(AggregateOperations.toList())
         .map(ToStopTime)
         .peek()
@@ -19,12 +36,18 @@ internal val stopTimes = Pipeline.create().apply {
         .writeTo(Sinks.map("stop_times"))
 }
 
-private fun pipeline(name: String, toJson: FunctionEx<List<String>, String?>) =
+private fun pipeline(name: String, columns: List<String>, fields: List<String>) =
     Pipeline.create().apply {
-        readFrom(file(name))
-            .apply(SplitByComma)
-            .map(toJson)
+        readFrom(file(name, columns))
+            .map(ToNameValuePair(fields))
+            .map(ToJson)
             .peek()
             .map(ToEntry)
             .writeTo(Sinks.map(name))
     }
+
+private fun file(filename: String, columns: List<String>) =
+    FileSources.files("${System.getProperty("data.path") ?: "/opt/hazelcast/data"}/infrastructure/data/current")
+        .glob("$filename.txt")
+        .format(FileFormat.csv(columns))
+        .build()
--- load-static/src/main/kotlin/com/hazelcast/jettrain/refs/Transforms.kt
+++ load-static/src/main/kotlin/com/hazelcast/jettrain/refs/Transforms.kt
@@ -1,17 +1,29 @@
 package com.hazelcast.jettrain.refs
 
 import com.hazelcast.function.FunctionEx
-import com.hazelcast.internal.json.Json
+import com.hazelcast.internal.json.JsonArray
 import com.hazelcast.internal.json.JsonObject
 import com.hazelcast.jet.Util
 import java.io.Serializable
 
-object ToEntry : FunctionEx<String?, Map.Entry<Serializable, Any>> {
-    override fun applyEx(string: String?): Map.Entry<Serializable, Any>? {
-        val id = string?.let(Json::parse)
-            ?.asObject()
-            ?.getString("id", null)
-        return if (id != null) Util.entry(id, string)
+class ToNameValuePair(private val fields: List<String>) : FunctionEx<Array<String>, List<Pair<String, String>>> {
+    override fun applyEx(values: Array<String>) = fields.zip(values)
+}
+
+object ToJson : FunctionEx<List<Pair<String, String>>, JsonObject> {
+    private val floatColumns = listOf("stop_long", "stop_lat")
+    override fun applyEx(pairs: List<Pair<String, String>>) =
+        pairs.fold(JsonObject()) { json, pair ->
+            if (floatColumns.contains(pair.first)) json.add(pair.first, pair.second.toFloat())
+            else json.add(pair.first, pair.second)
+            json
+        }
+}
+
+object ToEntry : FunctionEx<JsonObject, Map.Entry<Serializable, String>> {
+    override fun applyEx(json: JsonObject?): Map.Entry<Serializable, String>? {
+        val id = json?.getString("id", null)
+        return if (id != null) Util.entry(id, json.toString())
         else null
     }
 }
@@ -21,4 +33,20 @@ object ToEntryForStopTime : FunctionEx<JsonObject, Map.Entry<String, String>> {
         json.get("id").asString(),
         json.get("schedule").asArray().toString()
     )
+}
+
+object ToStopTime : FunctionEx<Map.Entry<String, List<Array<String>>>, JsonObject> {
+    override fun applyEx(entry: Map.Entry<String, List<Array<String>>>): JsonObject {
+        val wrapper = JsonObject().add("id", entry.key)
+        val schedule = entry.value.fold(JsonArray()) { array, element ->
+            val json = JsonObject().apply {
+                if (element.size > 1) add("departure", element[1])
+                if (element.size > 2) add("arrival", element[2])
+                if (element.size > 3) add("stopId", element[3])
+                if (element.size > 4) add("sequence", element[4])
+            }
+            array.add(json)
+        }
+        return wrapper.add("schedule", schedule)
+    }
 }
